80c22b41338268308a00090bd640888146fd5db4
rcalizayapa <rcalizayapa@unjbg.edu.pe>
2024-07-01 11:09:39 -0500

    Se agregaron las lineas de codigo a la Archivo Sumatoria.cpp

--- Sumatoria.cpp
+++ Sumatoria.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 int main(){
-    int n
+    int n, A[n], s, c;
+    float p;
+    s = 0;
+    c = 0;
+    cout << "Ingrese el numero de elementos: ";cin>>n;
+    for(int i = 0; i < n; i++){
+        cout << "Ingrese el elemento " << i+1 << ": ";cin>>A[i];
+        s = s + A[i];
+        
+        if(A[i] == 0){
+        	c = c + 1;
+            break;
+        }
+    }   
+    p = float(c*100)/n;
+    cout << "La sumatoria de los elementos es: " << s << endl;
+    cout << "El porcentaje de ceros ingresados es: " << p << "%" << endl;
 }
     
